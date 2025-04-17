e21da3df1645261c004ecc7828e951918a8f6854
Toni Kangas <toni.ar.kangas@gmail.com>
2025-04-17 03:00:33 +0300

    Re-add back-end test script

---
+++ back-end/test.sh
@@ -0,0 +1,41 @@
+#!/bin/sh -xe
+
+base_url=${1:-localhost\:5000}
+
+# Wait until server is online
+for i in $(seq 1 60); do
+    curl $base_url/question/thumbs && break || true
+    sleep 1
+done
+
+check_summary() {
+    len=$(curl -s $base_url/question/thumbs/answer | jq 'length')
+    test "$len" -eq $1
+
+    positive=$(curl -s $base_url/question/thumbs/summary | jq .'values."1"')
+    test "$positive" -eq $2
+    negative=$(curl -s $base_url/question/thumbs/summary | jq .'values."-1"')
+    test "$negative" -eq $3
+    empty=$(curl -s $base_url/question/thumbs/summary | jq .'values.""')
+    test "$empty" -eq $4
+}
+
+len=$(curl -s $base_url/question/thumbs/answer | jq 'length')
+test "$len" -eq 0
+
+feedback="1
+-1
+1"
+
+for i in $feedback; do
+    curl -s -X POST -H "Content-Type: application/json" -d "{\"value\":$i, \"submit\": true}" $base_url/question/thumbs/answer
+done;
+
+last=$(curl -s -X POST -H "Content-Type: application/json" -d "{\"value\": -1}" $base_url/question/thumbs/answer | jq .id -r)
+
+check_summary 4 2 2 0
+
+curl -s -X PATCH -H "Content-Type: application/json" -d "{\"value\": 1, \"submit\": true}" $base_url/answer/$last
+curl -s -X POST -H "Content-Type: application/json" -d "{}" $base_url/question/thumbs/answer
+
+check_summary 5 3 1 1
