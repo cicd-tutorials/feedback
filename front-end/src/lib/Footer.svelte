c6e7e0d950cd1d0ffb8b723d359e3fb5f399803b
Toni Kangas <toni.kangas@upcloud.com>
2025-04-23 23:54:08 +0300

    Add footer

---
+++ front-end/src/lib/Footer.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const build = import.meta.env.VITE_BUILD ?? "";
+</script>
+
+<footer>
+  <a href="https://github.com/cicd-tutorials/feedback.git" target="_blank">
+    Source code
+  </a>
+  {#if build}
+    <span>Build {build}</span>
+  {/if}
+</footer>
+
+<style>
+  footer {
+    display: flex;
+    justify-content: space-between;
+
+    color: var(--color-secondary);
+    font-size: 0.85rem;
+    border-top: 1px solid var(--color-border);
+    padding: 0.5rem 0;
+  }
+</style>
